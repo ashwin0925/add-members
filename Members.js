09190fa91051fcefb256d427b8180444c8315bcb
Ashwin Kumar <ashwinkumar9830@gmail.com>
2020-03-13 18:07:17 +0530

    first commit

---
+++ Members.js
@@ -0,0 +1,23 @@
+const members = [
+  {
+    id: 1,
+    name: 'john',
+    email: 'john@gmail.com',
+    status: 'active'
+  },
+  {
+    id: 2,
+    name: 'john cena',
+    email: 'johncena@gmail.com',
+    status: 'active'
+  }
+  ,
+  {
+    id: 3,
+    name: 'cena',
+    email: 'johncena@gmail.com',
+    status: 'inactive'
+  }
+]
+
+module.exports = members 
